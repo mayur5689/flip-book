57d2a67cbf6d65cc7b932b5b58bf686218ca7fcd
mayur5689 <mayur123roger@gmail.com>
2025-03-17 22:57:00 +0530

    Add images to pages 1 and 2

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,6 +5,26 @@ interface PageProps {
 }
 
 const Page: React.FC<PageProps> = React.forwardRef<HTMLDivElement, PageProps>((props, ref) => {
+  // For pages 1 and 2, display the corresponding images
+  if (props.number === 1 || props.number === 2) {
+    return (
+      <div className="page" ref={ref}>
+        <div className="page-content">
+          <h2>Page {props.number}</h2>
+          <div 
+            className="page-image"
+            style={{ 
+              backgroundImage: `url('/image/page${props.number}.jpg')`,
+              height: '80%'
+            }}
+          ></div>
+          <div className="page-footer">{props.number + 1}</div>
+        </div>
+      </div>
+    );
+  }
+  
+  // For other pages, use the default layout
   return (
     <div className="page" ref={ref}>
       <div className="page-content">
